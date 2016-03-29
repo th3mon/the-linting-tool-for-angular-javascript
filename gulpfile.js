cdee2c44d57c485fa1522cd839fcc7d20a0eb953
Przemysław Szelenberger <Przemyslaw.Szelenberger@ingbank.pl>
2016-03-29 10:54:06 +0200

    Refaktoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,38 @@
-﻿var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var stylish = require('jshint-stylish');
-var angularPlugin = require('eslint-plugin-angular');
-var config =
-    {
-        apps: ['app/**/*.js'],
-        html: ['*.html', 'app/**/*.html']
-    };
-
+﻿var
+  gulp = require('gulp'),
+  eslint = require('gulp-eslint'),
+  stylish = require('jshint-stylish'),
+  angularPlugin = require('eslint-plugin-angular'),
+  config = {
+    apps: ['app/**/*.js'],
+    html: ['*.html', 'app/**/*.html']
+  };
 
 gulp.task('validateSyntax-eslint', [], function () {
-    return gulp.src(config.apps)
-    .pipe(eslint(
-        {
-            "extends": ["eslint:recommended"],
-            "plugins": ["angular"],
-            "env": {
-                "browser": true
-            },
+  return gulp.src(config.apps)
+  .pipe(eslint({
+    extends: ['eslint:recommended'],
+    plugins: ['angular'],
 
-            "globals": {
-                'jQuery': true,
-                '$': true,
-                "angular": true,
-                "app":true
-            },
-            'rules': {
-                'quotes': [0, 'single'],
-                'semi': [2, 'always'],
-                'no-unused-vars': 1,
-                'no-console': 0,
-                "angular/controller-name": [1, "/[A-Z].*Controller$/"],
+    env: {
+        browser: true
+    },
 
+    globals: {
+      jQuery: true,
+      $: true,
+      angular: true,
+      app: true
+    },
 
-            }
-        }))
-    .pipe(eslint.format())
+    rules: {
+      quotes: [0, 'single'],
+      semi: [2, 'always'],
+      'no-unused-vars': 1,
+      'no-console': 0,
+      'angular/controller-name': [1, '/[A-Z].*Controller$/']
+    }
+  }))
+  .pipe(eslint.format())
 });
 
